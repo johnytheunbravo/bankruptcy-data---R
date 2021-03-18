970ed7092d8458fa5accb2cac395bfc44ae2e8f5
johnytheunbravo <77145974+johnytheunbravo@users.noreply.github.com>
2021-03-18 14:04:43 +0530

    vgjhvc
    Co-Authored-By: sushreeannanya <80768782+sushreeannanya@users.noreply.github.com>

--- bankruptcy data.R
+++ bankruptcy data.R
@@ -1,13 +1,12 @@
 # bankruptcy data
 # Tanishi Gupta 19/888
 # Sushree Annanya 19/886
-bankruptcy<- read.csv("data.csv",header = TRUE)
-View(bankruptcy)
-head(bankruptcy)
 summary(bankruptcy)
-bank<-subset(bankruptcy, Bankrupt.==1,select = c("Fixed.Assets.Turnover.Frequency","Total.Asset.Growth.Rate","Cash.Reinvestment..","Research.and.development.expense.rate","Interest.bearing.debt.interest.rate"))
-View(bank)
-summary(bank)
-nobank<-subset(bankruptcy, Bankrupt.==0,select = c("Fixed.Assets.Turnover.Frequency","Total.Asset.Growth.Rate","Cash.Reinvestment..","Research.and.development.expense.rate","Interest.bearing.debt.interest.rate"))
-View(nobank)
-
+train<-read.csv("aug_train.csv")
+View(train)
+male<-subset(train,gender=="Male")
+View(male)
+summary(male)
+female<-subset(train,gender=="Female")
+View(female)
+summary(female)
